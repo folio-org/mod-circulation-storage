8098f950323698e239970e6f4c7db34aa52831f5
Marc Johnson <marc@k-int.co.uk>
2017-03-02 11:38:07 +0000

    Run external database tests first during full build

--- full-build.sh
+++ full-build.sh
@@ -2,24 +2,20 @@
 
 ./setup-test-db.sh
 
-mvn -q compile -Dmaven.test.skip=true
+mvn -q test \
+    -Dorg.folio.loan.storage.test.database=external
+
+external_test_results=$?
 
 mvn -q test \
     -Dorg.folio.loan.storage.test.database=embedded
 
 embedded_test_results=$?
 
-./setup-test-db.sh
-
-mvn -q test \
-    -Dorg.folio.loan.storage.test.database=external
-
-external_test_results=$?
-
 mvn package -Dmaven.test.skip=true
 
-echo "Embedded database tests exit code: ${embedded_test_results}"
 echo "External database tests exit code: ${external_test_results}"
+echo "Embedded database tests exit code: ${embedded_test_results}"
 
 if [ $embedded_test_results != 0 ] || [ $external_test_results != 0 ]; then
     echo '--------------------------------------'
