dd054bbf5f8ac936e8941dbb703d899255af822f
Marc Johnson <marc@k-int.co.uk>
2017-03-02 09:41:57 +0000

    Script to initialise test database

---
+++ database-setup/setup-db.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+database_name=${1:-}
+schema_name=${2:-}
+user_name=${3:-}
+host=${4:-localhost}
+port=${5:-5432}
+executing_user=${6:-$USER}
+executing_password=${7:-}
+
+admin_user_name="loan_storage_admin"
+admin_password="admin"
+
+echo "Executing username: ${executing_user} password: ${executing_password}"
+
+./drop-schema.sh ${database_name} ${schema_name} ${host} ${port} ${executing_user} ${executing_password}
+./drop-db.sh ${database_name} ${host} ${port} ${executing_user} ${executing_password}
+./drop-role.sh ${user_name} ${host} ${port} ${executing_user} ${executing_password}
+
+./create-admin-role.sh ${admin_user_name} ${admin_password} ${host} ${port} ${executing_user} ${executing_password}
+./create-db.sh ${database_name} ${admin_user_name} ${host} ${port} ${executing_user} ${executing_password}
