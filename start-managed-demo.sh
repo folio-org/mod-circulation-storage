06e003cff4752293e03a2a46ff06ad347381aefd
Marc Johnson <marc@k-int.co.uk>
2017-03-03 09:53:58 +0000

    Create a tenant for a local demo

---
+++ start-managed-demo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+okapi_proxy_address=${1:-http://localhost:9130}
+
+echo "Check if Okapi is contactable"
+curl -w '\n' -X GET -D -   \
+     "${okapi_proxy_address}/_/env" || exit 1
+
+./create-tenant.sh
---
+++ stop-managed-demo.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+./delete-tenant.sh
+
