0cba46c2956ee72a3671c7755a955b85b809e043
Marc Johnson <marc@k-int.co.uk>
2017-06-13 17:05:05 +0100

    Add single sample loan policy

--- sample-data/import.sh
+++ sample-data/import.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 loan_storage_address=${1:-http://localhost:9130/loan-storage/loans}
-tenant=${2:-demo_tenant}
+loan_policy_storage_address=${2:-http://localhost:9130/loan-policy-storage/loan-policies}
+tenant=${3:-demo_tenant}
 
 for f in ./loans/*.json; do
     curl -w '\n' -X POST -D - \
@@ -9,5 +10,13 @@ for f in ./loans/*.json; do
          -H "X-Okapi-Tenant: ${tenant}" \
          -d @$f \
          "${loan_storage_address}"
+done
+
+for f in ./loan-policies/*.json; do
+    curl -w '\n' -X POST -D - \
+         -H "Content-type: application/json" \
+         -H "X-Okapi-Tenant: ${tenant}" \
+         -d @$f \
+         "${loan_policy_storage_address}"
 done
 
