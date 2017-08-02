98194d7b8c73542e16b05c4197f0bd4e4c89d27c
Marc Johnson <marc@k-int.co.uk>
2017-08-02 14:38:42 +0100

    Include requests in sample data

--- sample-data/import.sh
+++ sample-data/import.sh
@@ -2,7 +2,8 @@
 
 loan_storage_address=${1:-http://localhost:9130/loan-storage/loans}
 loan_policy_storage_address=${2:-http://localhost:9130/loan-policy-storage/loan-policies}
-tenant=${3:-demo_tenant}
+request_storage_address=${3:-http://localhost:9130/loan-policy-storage/loan-policies}
+tenant=${4:-demo_tenant}
 
 for f in ./loans/*.json; do
     curl -w '\n' -X POST -D - \
@@ -18,5 +19,13 @@ for f in ./loan-policies/*.json; do
          -H "X-Okapi-Tenant: ${tenant}" \
          -d @$f \
          "${loan_policy_storage_address}"
+done
+
+for f in ./requests/*.json; do
+    curl -w '\n' -X POST -D - \
+         -H "Content-type: application/json" \
+         -H "X-Okapi-Tenant: ${tenant}" \
+         -d @$f \
+         "${request_storage_address}"
 done
 
