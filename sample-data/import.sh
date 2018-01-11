f15b3e7b9636a0f49dc2e84a61ea2e56f51f7637
Marc Johnson <marc@k-int.co.uk>
2018-01-11 15:53:54 +0000

    Add sample loan rules CIRC-51

--- sample-data/import.sh
+++ sample-data/import.sh
@@ -2,16 +2,9 @@
 
 loan_storage_address=${1:-http://localhost:9130/loan-storage/loans}
 loan_policy_storage_address=${2:-http://localhost:9130/loan-policy-storage/loan-policies}
-request_storage_address=${3:-http://localhost:9130/request-storage/requests}
-tenant=${4:-demo_tenant}
-
-for f in ./loans/*.json; do
-    curl -w '\n' -X POST -D - \
-         -H "Content-type: application/json" \
-         -H "X-Okapi-Tenant: ${tenant}" \
-         -d @$f \
-         "${loan_storage_address}"
-done
+loan_rules_storage_address=${3:-http://localhost:9130/loan-rules-storage}
+request_storage_address=${4:-http://localhost:9130/request-storage/requests}
+tenant=${5:-demo_tenant}
 
 for f in ./loan-policies/*.json; do
     curl -w '\n' -X POST -D - \
@@ -19,6 +12,20 @@ for f in ./loan-policies/*.json; do
          -H "X-Okapi-Tenant: ${tenant}" \
          -d @$f \
          "${loan_policy_storage_address}"
+done
+
+curl -w '\n' -X PUT -D - \
+     -H "Content-type: application/json" \
+     -H "X-Okapi-Tenant: ${tenant}" \
+     -d @./loan-rules/use-example-for-everything.json \
+     "${loan_rules_storage_address}"
+
+for f in ./loans/*.json; do
+    curl -w '\n' -X POST -D - \
+         -H "Content-type: application/json" \
+         -H "X-Okapi-Tenant: ${tenant}" \
+         -d @$f \
+         "${loan_storage_address}"
 done
 
 for f in ./requests/*.json; do
