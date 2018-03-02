842ad54855f743bc3b552c324ec55101c70e1f26
John Malconian <malc@indexdata.com>
2018-03-02 17:10:29 +0000

    enhanced ref data import script which can optionally use an okapi auth token

--- reference-data/import.sh
+++ reference-data/import.sh
@@ -1,22 +1,90 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+#
+# import mod-circulation-storage reference data
+#
+
+#set -x
+
+type curl >/dev/null 2>&1 || { echo >&2 "$0: curl is required but it's not installed"; exit 1; }
+
+usage() {
+   cat << EOF
+Usage: ${0##*/} -o OKAPI_URL -t TENANT -a
+       -o Okapi_URL (required)
+       -t tenant (required)
+       -d directory_to_reference_data (optional) Default: '.'
+       -a x-okapi-token  (optional) Default: (no auth)
+EOF
+}
 
 
-loan_policy_storage_address=${1:-http://localhost:9130/loan-policy-storage/loan-policies}
-loan_rules_storage_address=${2:-http://localhost:9130/loan-rules-storage}
-tenant=${3:-demo_tenant}
+OPTIND=1 
 
-for f in ./loan-policy-storage/loan-policies/*.json; do
-    curl -w '\n' -X POST -D - \
-         -H "Content-type: application/json" \
-         -H "X-Okapi-Tenant: ${tenant}" \
-         -d @$f \
-         "${loan_policy_storage_address}"
+while getopts "h?a:o:t:d:" opt 
+do
+    case "$opt" in
+    h|\?)
+        usage
+        exit 1
+        ;;
+    o)  
+        okapiUrl=$OPTARG
+        ;;
+    t)  
+        tenant=$OPTARG
+        ;;
+    d)  
+        dataDirs+=("$OPTARG")
+        ;;
+    a)  
+        auth_required=true
+        authToken=$OPTARG
+        ;;  
+    *)  
+        usage >&2
+        exit 1
+        ;;
+    esac
 done
 
-curl -w '\n' -X PUT -D - \
-     -H "Content-type: application/json" \
-     -H "X-Okapi-Tenant: ${tenant}" \
-     -d @./loan-rules-storage/use-example-for-everything.json \
-     "${loan_rules_storage_address}"
+shift $((OPTIND -1))
 
+okapiUrl=${okapiUrl:-http://localhost:9130}
+tenant=${tenant:-demo_tenant}
+dataDirs=${dataDirs:-'.'}
+modEndpoints='loan-policy-storage/loan-policies loan-rules-storage'
+
+curlBaseOpts="-s -w '\n' --connect-timeout 10 \
+ -H 'Content-type: application/json' \
+ -H 'Accept: application/json' \
+ -H 'X-Okapi-Tenant: $tenant'"
+
+if [ "$auth_required" = true ]; then
+  authOpt="-H 'X-Okapi-Token: $authToken'"
+  curlOpts="$curlBaseOpts $authOpt"
+else
+  curlOpts=$curlBaseOpts
+fi
+
+for dir in "${dataDirs[@]}"; 
+do
+  for endpoint in $modEndpoints 
+  do 
+    if [ -d "${dir}/${endpoint}" ]; then
+      echo "ENDPOINT: $endpoint"
+      if [[ "$endpoint" =~ ^loan-rules-storage$ ]]; then
+        method=PUT
+      else
+        method=POST
+      fi
+
+      json=$(ls ${dir}/${endpoint}/*.json)
+      for j in $json 
+      do 
+        echo "curl $curlOpts -X $method -d @$j ${okapiUrl}/${endpoint}"
+      done
+    fi
+  done
+done
 
