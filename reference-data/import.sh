06b3ba4ec750d23d680425c1740668f1e3c35d00
John Malconian <malc@indexdata.com>
2018-03-02 16:11:46 +0000

    update reference data directory names to match their endpoints

--- reference-data/import.sh
+++ reference-data/import.sh
@@ -5,7 +5,7 @@ loan_policy_storage_address=${1:-http://localhost:9130/loan-policy-storage/loan-
 loan_rules_storage_address=${2:-http://localhost:9130/loan-rules-storage}
 tenant=${3:-demo_tenant}
 
-for f in ./loan-policies/*.json; do
+for f in ./loan-policy-storage/loan-policies/*.json; do
     curl -w '\n' -X POST -D - \
          -H "Content-type: application/json" \
          -H "X-Okapi-Tenant: ${tenant}" \
@@ -16,7 +16,7 @@ done
 curl -w '\n' -X PUT -D - \
      -H "Content-type: application/json" \
      -H "X-Okapi-Tenant: ${tenant}" \
-     -d @./loan-rules/use-example-for-everything.json \
+     -d @./loan-rules-storage/use-example-for-everything.json \
      "${loan_rules_storage_address}"
 
 
