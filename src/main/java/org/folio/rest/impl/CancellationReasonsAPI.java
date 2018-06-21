59186839136cbb62ed328c5a6db260abbf0264b5
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2018-06-21 15:07:01 +0000

    Do initial implementation and tests for cancellationReasons crud

---
+++ src/main/java/org/folio/rest/impl/CancellationReasonsAPI.java
@@ -0,0 +1,319 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.folio.rest.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import javax.ws.rs.core.Response;
+import org.folio.rest.annotations.Validate;
+import org.folio.rest.jaxrs.model.CancellationReason;
+import org.folio.rest.jaxrs.resource.CancellationReasonStorageResource;
+import org.folio.rest.persist.Criteria.Limit;
+import org.folio.rest.persist.Criteria.Offset;
+import org.folio.rest.persist.cql.CQLWrapper;
+import org.z3950.zing.cql.cql2pgjson.CQL2PgJSON;
+
+import static org.folio.rest.impl.Headers.TENANT_HEADER;
+import org.folio.rest.jaxrs.model.CancellationReasons;
+import org.folio.rest.persist.Criteria.Criteria;
+import org.folio.rest.persist.Criteria.Criterion;
+import org.folio.rest.persist.PgExceptionUtil;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.tools.PomReader;
+import org.folio.rest.tools.utils.OutStream;
+
+/**
+ *
+ * @author kurt
+ */
+public class CancellationReasonsAPI implements CancellationReasonStorageResource {
+  
+  private static final Logger logger = LoggerFactory.getLogger(CancellationReasonsAPI.class);
+  private static final String TABLE_NAME = "cancellation_reason";
+  private boolean suppressErrorResponse = false;
+  private static final String ID_FIELD = "'id'";
+  
+  private CQLWrapper getCQL(String query, int limit, int offset) throws Exception {
+    CQL2PgJSON cql2pgJson = new CQL2PgJSON(TABLE_NAME + ".jsonb");
+    return new CQLWrapper(cql2pgJson, query).setLimit(new Limit(limit))
+        .setOffset(new Offset(offset));
+  }
+  
+  private String logAndSaveError(Throwable err) {
+    String message = err.getLocalizedMessage();
+    logger.error(message, err);
+    return message;
+  }
+  
+  private String getErrorResponse(String response) {
+    if(suppressErrorResponse) {
+      return "Internal Server Error: Please contact Admin";
+    }
+    return response;
+  }
+
+  private boolean isDuplicate(String errorMessage){
+    if(errorMessage != null && errorMessage.contains("duplicate key value violates unique constraint")){
+      return true;
+    }
+    return false;
+  }
+
+  private boolean isInvalidUUID(String errorMessage){
+    if(errorMessage != null && errorMessage.contains("invalid input syntax for uuid")){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  @Override
+  public void deleteCancellationReasonStorageCancellationReasons(String lang, 
+      Map<String, String> okapiHeaders, 
+      Handler<AsyncResult<Response>> asyncResultHandler,
+      Context vertxContext) throws Exception {
+    try {
+      String tenantId = okapiHeaders.get(TENANT_HEADER);
+      String deleteAllQuery = String.format("DELETE FROM %s_%s.%s", tenantId,
+          PomReader.INSTANCE.getModuleName(), TABLE_NAME);
+      PostgresClient.getInstance(vertxContext.owner(), tenantId).mutate(deleteAllQuery,
+          mutateReply -> {
+        if(mutateReply.failed()) {
+          String message = logAndSaveError(mutateReply.cause());
+          asyncResultHandler.handle(Future.succeededFuture(
+              DeleteCancellationReasonStorageCancellationReasonsResponse
+              .withPlainInternalServerError(getErrorResponse(message))));     
+        } else {
+          asyncResultHandler.handle(Future.succeededFuture(
+              DeleteCancellationReasonStorageCancellationReasonsResponse
+              .noContent().build()));
+        }
+      });
+    } catch(Exception e) {
+      String message = logAndSaveError(e);
+      asyncResultHandler.handle(Future.succeededFuture(
+          DeleteCancellationReasonStorageCancellationReasonsResponse
+          .withPlainInternalServerError(getErrorResponse(message))));     
+    }
+  }
+
+  @Override
+  public void getCancellationReasonStorageCancellationReasons(int offset,
+      int limit, String query, String lang, Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext)
+      throws Exception {
+    try {
+      String tenantId = okapiHeaders.get(TENANT_HEADER);
+      CQLWrapper cql = getCQL(query, limit, offset);
+      PostgresClient.getInstance(vertxContext.owner(), tenantId)
+          .get(TABLE_NAME, CancellationReason.class, new String[]{"*"},
+          cql, true, true, getReply -> {
+        try {
+          if(getReply.failed()) {
+            String message = logAndSaveError(getReply.cause());
+            asyncResultHandler.handle(Future.succeededFuture(
+                GetCancellationReasonStorageCancellationReasonsResponse
+                .withPlainInternalServerError(getErrorResponse(message))));              
+          } else {
+            CancellationReasons collection = new CancellationReasons();
+            List<CancellationReason> crList = (List<CancellationReason>) 
+                getReply.result().getResults();
+            collection.setCancellationReasons(crList);
+            collection.setTotalRecords(getReply.result().getResultInfo().getTotalRecords());
+            asyncResultHandler.handle(Future.succeededFuture(
+                GetCancellationReasonStorageCancellationReasonsResponse
+                .withJsonOK(collection)));               
+          }
+        } catch(Exception e) {
+          String message = logAndSaveError(e);
+          asyncResultHandler.handle(Future.succeededFuture(
+              GetCancellationReasonStorageCancellationReasonsResponse
+              .withPlainInternalServerError(getErrorResponse(message))));          
+        }     
+      });
+    } catch(Exception e) {
+      String message = logAndSaveError(e);
+      asyncResultHandler.handle(Future.succeededFuture(
+          GetCancellationReasonStorageCancellationReasonsResponse
+          .withPlainInternalServerError(getErrorResponse(message))));      
+    }
+  }
+
+  @Override
+  @Validate
+  public void postCancellationReasonStorageCancellationReasons(String lang,
+      CancellationReason entity, Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext)
+      throws Exception {
+    try {
+      String tenantId = okapiHeaders.get(TENANT_HEADER);
+      String id = entity.getId();
+      if(id == null) {
+        id = UUID.randomUUID().toString();
+        entity.setId(id);
+      }
+      PostgresClient pgClient = PostgresClient.getInstance(vertxContext.owner(),
+          tenantId);
+      pgClient.save(TABLE_NAME, id, entity, reply -> {
+        try {
+          if(reply.failed()) {
+            String message = logAndSaveError(reply.cause());
+            if(isDuplicate(message)) {
+              asyncResultHandler.handle(Future.succeededFuture(
+                  PostCancellationReasonStorageCancellationReasonsResponse
+                  .withPlainBadRequest(PgExceptionUtil.badRequestMessage(reply.cause()))));
+            } else {
+              asyncResultHandler.handle(Future.succeededFuture(
+                  PostCancellationReasonStorageCancellationReasonsResponse
+                  .withPlainInternalServerError(getErrorResponse(message))));
+            }
+          } else {
+            OutStream stream = new OutStream();
+            stream.setData(entity);
+            asyncResultHandler.handle(Future.succeededFuture(
+                PostCancellationReasonStorageCancellationReasonsResponse
+                .withJsonCreated(reply.result(), stream)));
+          }
+        } catch(Exception e) {
+          String message = logAndSaveError(e);
+          asyncResultHandler.handle(Future.succeededFuture(
+              PostCancellationReasonStorageCancellationReasonsResponse
+              .withPlainInternalServerError(getErrorResponse(message))));
+        }
+      });
+    } catch(Exception e) {
+      String message = logAndSaveError(e);
+      asyncResultHandler.handle(Future.succeededFuture(
+          PostCancellationReasonStorageCancellationReasonsResponse
+          .withPlainInternalServerError(getErrorResponse(message))));
+    }
+  }
+
+  @Override
+  public void getCancellationReasonStorageCancellationReasonsByCancellationReasonId(
+      String cancellationReasonId, String lang, Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext)
+      throws Exception {
+    try {
+      Criteria idCrit = new Criteria().setOperation("=")
+          .setValue(cancellationReasonId).addField(ID_FIELD);
+      String tenantId = okapiHeaders.get(TENANT_HEADER);
+      PostgresClient.getInstance(vertxContext.owner(), tenantId).get(TABLE_NAME,
+          CancellationReason.class, new Criterion(idCrit), true, false, getReply -> {
+        if(getReply.failed()) {
+          String message = logAndSaveError(getReply.cause());
+          asyncResultHandler.handle(Future.succeededFuture(
+              GetCancellationReasonStorageCancellationReasonsByCancellationReasonIdResponse
+              .withPlainInternalServerError(getErrorResponse(message))));
+        } else {
+          List<CancellationReason> reasons = (List<CancellationReason>) getReply.result().getResults();
+          if(reasons.isEmpty()) {
+            asyncResultHandler.handle(Future.succeededFuture(
+              GetCancellationReasonStorageCancellationReasonsByCancellationReasonIdResponse
+                  .withPlainNotFound("No record with that id")));
+          } else {
+           asyncResultHandler.handle(Future.succeededFuture(
+              GetCancellationReasonStorageCancellationReasonsByCancellationReasonIdResponse
+                  .withJsonOK(reasons.get(0))));
+          }
+        }
+      });
+    } catch(Exception e) {
+      String message = logAndSaveError(e);
+      asyncResultHandler.handle(Future.succeededFuture(
+          GetCancellationReasonStorageCancellationReasonsByCancellationReasonIdResponse
+          .withPlainInternalServerError(getErrorResponse(message))));
+    }
+  }
+
+  @Override
+  public void deleteCancellationReasonStorageCancellationReasonsByCancellationReasonId(
+      String cancellationReasonId, String lang, Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext)
+      throws Exception {
+    try {
+      Criteria idCrit = new Criteria().setOperation("=")
+          .setValue(cancellationReasonId).addField(ID_FIELD);
+      String tenantId = okapiHeaders.get(TENANT_HEADER);
+      PostgresClient.getInstance(vertxContext.owner(), tenantId).delete(TABLE_NAME,
+          new Criterion(idCrit), deleteHandler -> {
+        if(deleteHandler.failed()) {
+          String message = logAndSaveError(deleteHandler.cause());
+          asyncResultHandler.handle(Future.succeededFuture(
+              DeleteCancellationReasonStorageCancellationReasonsByCancellationReasonIdResponse
+              .withPlainInternalServerError(getErrorResponse(message))));
+        } else {
+          if(deleteHandler.result().getUpdated() < 1) {
+            asyncResultHandler.handle(Future.succeededFuture(
+              DeleteCancellationReasonStorageCancellationReasonsByCancellationReasonIdResponse
+                  .withPlainNotFound("Record not found")));
+          } else {
+            asyncResultHandler.handle(Future.succeededFuture(
+              DeleteCancellationReasonStorageCancellationReasonsByCancellationReasonIdResponse
+                  .withNoContent()));
+          }         
+        }
+      });
+    } catch(Exception e) {
+      String message = logAndSaveError(e);
+      asyncResultHandler.handle(Future.succeededFuture(
+          DeleteCancellationReasonStorageCancellationReasonsByCancellationReasonIdResponse
+          .withPlainInternalServerError(getErrorResponse(message))));      
+    }
+  }
+
+  @Override
+  @Validate
+  public void putCancellationReasonStorageCancellationReasonsByCancellationReasonId(
+      String cancellationReasonId, String lang, CancellationReason entity,
+      Map<String, String> okapiHeaders,
+      Handler<AsyncResult<Response>> asyncResultHandler,
+      Context vertxContext) throws Exception {
+    try {
+      Criteria idCrit = new Criteria().setOperation("=")
+          .setValue(cancellationReasonId).addField(ID_FIELD);
+      String tenantId = okapiHeaders.get(TENANT_HEADER);
+      PostgresClient.getInstance(vertxContext.owner(), tenantId).update(TABLE_NAME, 
+          entity, new Criterion(idCrit), true, putHandler -> {            
+        if(putHandler.failed()) {
+          String message = logAndSaveError(putHandler.cause());
+          if(isDuplicate(message)) {
+            asyncResultHandler.handle(Future.succeededFuture(
+                PutCancellationReasonStorageCancellationReasonsByCancellationReasonIdResponse
+                .withPlainBadRequest(PgExceptionUtil.badRequestMessage(putHandler.cause()))));            
+          } else {
+            asyncResultHandler.handle(Future.succeededFuture(
+                PutCancellationReasonStorageCancellationReasonsByCancellationReasonIdResponse
+                .withPlainInternalServerError(getErrorResponse(message))));
+          }
+        } else {
+          if(putHandler.result().getUpdated() < 1) {
+            asyncResultHandler.handle(Future.succeededFuture(
+                PutCancellationReasonStorageCancellationReasonsByCancellationReasonIdResponse
+                .withPlainNotFound("Record not found")));
+          } else {
+            asyncResultHandler.handle(Future.succeededFuture(
+                PutCancellationReasonStorageCancellationReasonsByCancellationReasonIdResponse
+                .withNoContent()));
+          }
+        }
+      });
+    } catch(Exception e) {
+      String message = logAndSaveError(e);
+      asyncResultHandler.handle(Future.succeededFuture(
+          PutCancellationReasonStorageCancellationReasonsByCancellationReasonIdResponse
+          .withPlainInternalServerError(getErrorResponse(message))));
+    }
+  }  
+}
---
+++ src/test/java/org/folio/rest/api/CancellationReasonsApiTest.java
@@ -0,0 +1,293 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.folio.rest.api;
+
+import io.vertx.core.json.JsonObject;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import static org.folio.rest.api.FixedDueDateApiTest.dueDateURL;
+import static org.folio.rest.api.LoanPoliciesApiTest.loanPolicyStorageUrl;
+import org.folio.rest.support.ApiTests;
+import org.folio.rest.support.IndividualResource;
+import org.folio.rest.support.JsonResponse;
+import org.folio.rest.support.ResponseHandler;
+import static org.hamcrest.core.Is.is;
+import org.hamcrest.junit.MatcherAssert;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import static org.folio.rest.api.FixedDueDateApiTest.dueDateURL;
+import static org.folio.rest.api.LoanPoliciesApiTest.loanPolicyStorageUrl;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertTrue;
+import static org.folio.rest.api.FixedDueDateApiTest.dueDateURL;
+import static org.folio.rest.api.LoanPoliciesApiTest.loanPolicyStorageUrl;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertTrue;
+import static org.folio.rest.api.FixedDueDateApiTest.dueDateURL;
+import static org.folio.rest.api.LoanPoliciesApiTest.loanPolicyStorageUrl;
+import org.folio.rest.support.Response;
+import org.folio.rest.support.TextResponse;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ *
+ * @author kurt
+ */
+public class CancellationReasonsApiTest extends ApiTests {
+  @Before
+  public void beforeEach()
+    throws MalformedURLException {
+    StorageTestSuite.deleteAll(cancelReasonURL());
+  }
+  
+  protected static URL cancelReasonURL() throws MalformedURLException {
+    return cancelReasonURL("");
+  }
+
+  protected static URL cancelReasonURL(String subPath)
+    throws MalformedURLException {
+
+    return StorageTestSuite.storageUrl(
+      "/cancellation-reason-storage/cancellation-reasons" + subPath);
+  }
+  
+  private IndividualResource assertCreateCancellationReason(JsonObject request)
+      throws MalformedURLException,
+      InterruptedException,
+      ExecutionException,
+      TimeoutException {
+
+    JsonResponse response = createCancellationReason(request);
+    
+    MatcherAssert.assertThat(String.format("Failed to create cancellation reason: %s", response.getBody()),
+      response.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+
+    return new IndividualResource(response);
+  }
+  
+  private JsonResponse createCancellationReason(JsonObject request) 
+      throws MalformedURLException,
+      InterruptedException,
+      ExecutionException,
+      TimeoutException {
+    CompletableFuture<JsonResponse> createCompleted = new CompletableFuture<>();
+
+    client.post(cancelReasonURL(), request, StorageTestSuite.TENANT_ID,
+      ResponseHandler.json(createCompleted));
+
+    JsonResponse response = createCompleted.get(5, TimeUnit.SECONDS);
+    return response;
+  }
+  
+  private IndividualResource assertGetCancellationReason(String id) 
+      throws MalformedURLException,
+      InterruptedException, 
+      ExecutionException,
+      TimeoutException {    
+    JsonResponse response = getCancellationReason(id);
+    
+    MatcherAssert.assertThat(String.format("Failed to retrieve cancellation reason: %s (%s)",
+        response.getBody(), response.getStatusCode()),
+        response.getStatusCode(), is(HttpURLConnection.HTTP_OK));
+    
+    return new IndividualResource(response);
+  }
+  
+  private JsonResponse getCancellationReason(String id) 
+      throws MalformedURLException,
+      InterruptedException,
+      ExecutionException, 
+      TimeoutException {
+    CompletableFuture<JsonResponse> getReasonFuture = new CompletableFuture<>();    
+    client.get(cancelReasonURL("/"+id), StorageTestSuite.TENANT_ID,
+        ResponseHandler.json(getReasonFuture));
+    JsonResponse response = getReasonFuture.get(5, TimeUnit.SECONDS);
+    return response;
+  }
+  
+  private JsonResponse getCancellationReasonCollection(String query)
+      throws MalformedURLException,
+      InterruptedException,
+      ExecutionException, 
+      TimeoutException {
+    CompletableFuture<JsonResponse> getReasonsFuture = new CompletableFuture<>();
+    String queryParam;
+    if(query != null) {
+      queryParam = "?query=" + query;
+    } else {
+      queryParam = "";
+    }
+    client.get(cancelReasonURL(queryParam), StorageTestSuite.TENANT_ID,
+        ResponseHandler.json(getReasonsFuture));
+    JsonResponse response = getReasonsFuture.get(5, TimeUnit.SECONDS);
+    return response;
+  }
+  
+  private TextResponse updateCancellationReason(String id, JsonObject request) 
+      throws MalformedURLException,
+      InterruptedException,
+      ExecutionException, 
+      TimeoutException {
+    CompletableFuture<TextResponse> updateReasonFuture = new CompletableFuture<>();
+    client.put(cancelReasonURL("/"+id), request, StorageTestSuite.TENANT_ID,
+        ResponseHandler.text(updateReasonFuture));
+    TextResponse textResponse = updateReasonFuture.get(5, TimeUnit.SECONDS);
+    return textResponse;    
+  }
+  
+  private void assertUpdateCancellationReason(String id, JsonObject request)
+      throws MalformedURLException,
+      InterruptedException,
+      ExecutionException, 
+      TimeoutException {
+    TextResponse response = updateCancellationReason(id, request);
+    
+    MatcherAssert.assertThat(String.format("Failed to update cancellation reason: %s (%s)",
+        response.getBody(), response.getStatusCode()),
+        response.getStatusCode(), is(HttpURLConnection.HTTP_NO_CONTENT));
+  }
+  
+  private TextResponse deleteCancellationReason(String id)
+      throws MalformedURLException,
+      InterruptedException,
+      ExecutionException, 
+      TimeoutException {
+    CompletableFuture<TextResponse> deleteReasonFuture = new CompletableFuture<>();
+    client.delete(cancelReasonURL("/"+id), StorageTestSuite.TENANT_ID,
+        ResponseHandler.text(deleteReasonFuture));
+    TextResponse textResponse = deleteReasonFuture.get(5, TimeUnit.SECONDS);
+    return textResponse;
+  }
+  
+  private void assertDeleteCancellationReason(String id)
+      throws MalformedURLException,
+      InterruptedException,
+      ExecutionException, 
+      TimeoutException {
+    TextResponse response = deleteCancellationReason(id);
+    
+    MatcherAssert.assertThat(String.format("Failed to delete cancellation reason: %s (%s)",
+        response.getBody(), response.getStatusCode()),
+        response.getStatusCode(), is(HttpURLConnection.HTTP_NO_CONTENT));
+  }
+  
+  //Tests
+  @Test
+  public void canCreateCancellationReason() 
+      throws MalformedURLException, 
+      InterruptedException, 
+      ExecutionException, 
+      TimeoutException {
+    JsonObject request = new JsonObject()
+        .put("name", "cosmicrays")
+        .put("description", "Excess solar radiation has destroyed the item");
+    assertCreateCancellationReason(request);
+  }
+  
+  @Test
+  public void canCreateAndRetrieveCancellationRequest() 
+      throws MalformedURLException, 
+      InterruptedException,
+      ExecutionException,
+      TimeoutException {
+    String id = UUID.randomUUID().toString();
+    JsonObject request = new JsonObject()
+        .put("name", "slime")
+        .put("id", id)
+        .put("description", "Item slimed");
+    assertCreateCancellationReason(request);
+    IndividualResource reason = assertGetCancellationReason(id);
+    assertEquals(reason.getJson().getString("name"), "slime");
+  }
+  
+  @Test
+  public void canUpdateCancellationRequest()
+      throws MalformedURLException, 
+      InterruptedException,
+      ExecutionException,
+      TimeoutException {
+    String id = UUID.randomUUID().toString();
+    JsonObject request = new JsonObject()
+        .put("name", "slime")
+        .put("id", id)
+        .put("description", "Item slimed");
+    assertCreateCancellationReason(request);
+    request.put("name", "oobleck");
+    assertUpdateCancellationReason(id, request);
+    IndividualResource reason = assertGetCancellationReason(id);
+    assertEquals(reason.getJson().getString("name"), "oobleck");
+  }
+  
+  @Test
+  public void canRetrieveByCQL()
+      throws MalformedURLException, 
+      InterruptedException,
+      ExecutionException,
+      TimeoutException {
+    JsonObject request = new JsonObject()
+        .put("name", "ooze")
+        .put("description", "Item oozed");
+    JsonObject request2 = new JsonObject()
+        .put("name", "fire")
+        .put("description", "Item burnt");
+    assertCreateCancellationReason(request);
+    assertCreateCancellationReason(request2);
+    JsonResponse response = getCancellationReasonCollection("description=*burnt");
+    assertTrue(response.getJson().containsKey("totalRecords"));
+    assertTrue(response.getJson().getInteger("totalRecords").equals(1));
+  }
+  
+  @Test
+  public void canDeleteCancellationRequest()
+      throws MalformedURLException, 
+      InterruptedException,
+      ExecutionException,
+      TimeoutException {
+    String id = UUID.randomUUID().toString();
+    JsonObject request = new JsonObject()
+        .put("name", "meteor")
+        .put("id", id)
+        .put("description", "SMOD incoming, all requests cancelled");
+    assertCreateCancellationReason(request);
+    assertDeleteCancellationReason(id);
+    JsonResponse getResponse = getCancellationReason(id);
+    assertTrue(getResponse.getStatusCode() == 404);
+  }
+  
+  @Test
+  public void cannotCreateDuplicateCancellationRequestNames()
+      throws MalformedURLException, 
+      InterruptedException,
+      ExecutionException,
+      TimeoutException {
+    JsonObject request = new JsonObject()
+        .put("name", "chicken")
+        .put("description", "Giant chicken has eaten the item");
+    JsonObject request2 = new JsonObject()
+        .put("name", "chicken")
+        .put("description", "Chicken grease stains on item");
+    assertCreateCancellationReason(request);
+    JsonResponse response = createCancellationReason(request2);
+    assertEquals(400, response.getStatusCode());    
+  }
+  
+  @Test
+  //My canary in the coalmine :)
+  public void dummyTest() {
+    assertTrue(true);
+  }
+  
+  
+}
