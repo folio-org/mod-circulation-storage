8a04ccf87bc86c1029dba4e0f9ae278927d09f13
VRohach <Volodymyr_Rohach@epam.com>
2019-10-10 14:38:19 +0300

    CIRCSTORE-160: Create default overdue fines policy. URL for overdue fine policies changed.

--- reference-data/import.sh
+++ reference-data/import.sh
@@ -53,7 +53,7 @@ shift $((OPTIND -1))
 okapiUrl=${okapiUrl:-http://localhost:9130}
 tenant=${tenant:-demo_tenant}
 dataDirs=${dataDirs:-'.'}
-modEndpoints='loan-policy-storage/loan-policies request-policy-storage/request-policies patron-notice-policy-storage/patron-notice-policies circulation-rules-storage cancellation-reason-storage/cancellation-reasons overdue-fine-policy/overdue-fine-policies'
+modEndpoints='loan-policy-storage/loan-policies request-policy-storage/request-policies patron-notice-policy-storage/patron-notice-policies circulation-rules-storage cancellation-reason-storage/cancellation-reasons overdue-fines-policies'
 
 for dir in "${dataDirs[@]}";
 do
