ac28bbb1cdfcda981609ef64d4eab6c9c299e32c
Oleksandr Vidinieiev <Oleksandr_Vidinieiev@epam.com>
2019-11-18 13:25:07 +0200

    Added tests for item limit bounds validation

--- src/test/java/org/folio/rest/api/LoanPoliciesApiTest.java
+++ src/test/java/org/folio/rest/api/LoanPoliciesApiTest.java
@@ -315,6 +315,93 @@ public class LoanPoliciesApiTest extends ApiTests {
   }
 
   @Test
+  public void canCreateLoanPolicyWithItemLimitWithinBounds()
+    throws MalformedURLException,
+    InterruptedException,
+    ExecutionException,
+    TimeoutException {
+
+    CompletableFuture<JsonResponse> createCompleted = new CompletableFuture<>();
+
+    UUID id = UUID.randomUUID();
+
+    JsonObject loanPolicyRequest = defaultRollingPolicy()
+      .withId(id)
+      .withName("Example Loan Policy")
+      .withDescription("An example loan policy")
+      .create();
+
+    loanPolicyRequest.getJsonObject("loansPolicy").put("itemLimit", 1000);
+
+    client.post(loanPolicyStorageUrl(),
+      loanPolicyRequest, StorageTestSuite.TENANT_ID,
+      ResponseHandler.json(createCompleted));
+
+    JsonResponse response = createCompleted.get(5, TimeUnit.SECONDS);
+
+    assertThat(String.format("Failed to create loan policy: %s", response.getBody()),
+      response.getStatusCode(), is(HttpURLConnection.HTTP_CREATED));
+  }
+
+  @Test
+  public void cannotCreateLoanPolicyWithItemLimitBelowMinimum()
+    throws MalformedURLException,
+    InterruptedException,
+    ExecutionException,
+    TimeoutException {
+
+    CompletableFuture<JsonResponse> createCompleted = new CompletableFuture<>();
+
+    UUID id = UUID.randomUUID();
+
+    JsonObject loanPolicyRequest = defaultRollingPolicy()
+      .withId(id)
+      .withName("Example Loan Policy")
+      .withDescription("An example loan policy")
+      .create();
+
+    loanPolicyRequest.getJsonObject("loansPolicy").put("itemLimit", 0);
+
+    client.post(loanPolicyStorageUrl(),
+      loanPolicyRequest, StorageTestSuite.TENANT_ID,
+      ResponseHandler.json(createCompleted));
+
+    JsonResponse response = createCompleted.get(5, TimeUnit.SECONDS);
+
+    assertThat(String.format("Should fail to create loan policy: %s", response.getBody()),
+      response.getStatusCode(), is(HttpStatus.HTTP_INTERNAL_SERVER_ERROR.toInt()));
+  }
+
+  @Test
+  public void cannotCreateLoanPolicyWithItemLimitAboveMaximum()
+    throws MalformedURLException,
+    InterruptedException,
+    ExecutionException,
+    TimeoutException {
+
+    CompletableFuture<JsonResponse> createCompleted = new CompletableFuture<>();
+
+    UUID id = UUID.randomUUID();
+
+    JsonObject loanPolicyRequest = defaultRollingPolicy()
+      .withId(id)
+      .withName("Example Loan Policy")
+      .withDescription("An example loan policy")
+      .create();
+
+    loanPolicyRequest.getJsonObject("loansPolicy").put("itemLimit", 10_000);
+
+    client.post(loanPolicyStorageUrl(),
+      loanPolicyRequest, StorageTestSuite.TENANT_ID,
+      ResponseHandler.json(createCompleted));
+
+    JsonResponse response = createCompleted.get(5, TimeUnit.SECONDS);
+
+    assertThat(String.format("Should fail to create loan policy: %s", response.getBody()),
+      response.getStatusCode(), is(HttpStatus.HTTP_INTERNAL_SERVER_ERROR.toInt()));
+  }
+
+  @Test
   public void cannotCreateALoanPolicyWithInvalidPeriodInterval()
     throws MalformedURLException,
     InterruptedException,
