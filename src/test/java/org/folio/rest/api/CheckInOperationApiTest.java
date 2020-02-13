3e0cb6376e4e90d725f001304ded1a4131a77138
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-02-13 16:29:33 +0200

    CIRCSTORE-193: Use datetime timestamp instead of string.

--- src/test/java/org/folio/rest/api/CheckInOperationApiTest.java
+++ src/test/java/org/folio/rest/api/CheckInOperationApiTest.java
@@ -112,7 +112,7 @@ public class CheckInOperationApiTest extends ApiTests {
     checkInOperationClient.create(thirdCheckInOperation);
 
     MultipleRecords<JsonObject> allCheckInOperations = checkInOperationClient
-      .getMany(String.format("occurredDateTime >= '%s'", beforeCreateDateTime));
+      .getMany(String.format("occurredDateTime >= %s", beforeCreateDateTime.getMillis()));
 
     assertThat(allCheckInOperations.getTotalRecords(), is(3));
     assertTrue(allCheckInOperations.getRecords().contains(firstCheckInOperation));
