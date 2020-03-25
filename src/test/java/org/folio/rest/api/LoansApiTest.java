fcf4e44cb3ef1f2bca82eeb2ddde929ab98298c7
Svitlana-Zmiivska <Svitlana_Zmiivska@epam.com>
2020-03-25 15:23:27 +0200

    CIRCSTORE-201 Add markedMissing property to a loan

--- src/test/java/org/folio/rest/api/LoansApiTest.java
+++ src/test/java/org/folio/rest/api/LoansApiTest.java
@@ -86,6 +86,7 @@ public class LoansApiTest extends ApiTests {
     UUID overdueFinePolicyId = UUID.randomUUID();
     UUID lostItemPolicyId = UUID.randomUUID();
     final DateTime claimedReturnedDate = DateTime.now(DateTimeZone.UTC);
+    final DateTime markedMissingDate = DateTime.now(DateTimeZone.UTC);
 
     JsonObject loanRequest = new LoanRequestBuilder()
       .withId(id)
@@ -103,6 +104,7 @@ public class LoansApiTest extends ApiTests {
       .withOverdueFinePolicyId(overdueFinePolicyId)
       .withLostItemPolicyId(lostItemPolicyId)
       .withClaimedReturnedDate(claimedReturnedDate)
+      .withMarkedMissingDate(markedMissingDate)
       .create();
 
     JsonObject loan = loansClient.create(loanRequest).getJson();
@@ -157,6 +159,9 @@ public class LoansApiTest extends ApiTests {
 
     assertThat(DateTime.parse(loan.getString("claimedReturnedDate")),
       is(claimedReturnedDate));
+
+    assertThat(DateTime.parse(loan.getString("markedMissingDate")),
+      is(markedMissingDate));
   }
 
   @Test
