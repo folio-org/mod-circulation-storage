ff7e1711d4d26f970d2108215f60b69527a133f9
Oleksandr Yatsenko <Oleksandr_Yatsenko1@epam.com>
2021-01-22 16:50:42 +0200

    Updated extending of tenant API to load data in asynchronous way.

--- src/main/java/org/folio/rest/impl/TenantRefAPI.java
+++ src/main/java/org/folio/rest/impl/TenantRefAPI.java
@@ -1,6 +1,8 @@
 package org.folio.rest.impl;
+import io.vertx.core.Future;
 import java.util.Map;
 import javax.ws.rs.core.Response;
+import org.folio.rest.annotations.Validate;
 import org.folio.rest.jaxrs.model.TenantAttributes;
 
 import io.vertx.core.AsyncResult;
@@ -13,8 +15,40 @@ import org.folio.rest.tools.utils.TenantLoading;
 import org.folio.service.PubSubRegistrationService;
 
 public class TenantRefAPI extends TenantAPI {
-  private static final Logger log = LogManager.getLogger();
 
+  private static final Logger log = LogManager.getLogger();
+  public static final String REFERENCE_KEY = "loadReference";
+  public static final String REFERENCE_LEAD = "ref-data";
+  public static final String SAMPLE_KEY = "loadSample";
+  public static final String SAMPLE_LEAD = "sample-data";
+
+  @Validate
+  @Override
+  Future<Integer> loadData(TenantAttributes attributes, String tenantId,
+      Map<String, String> headers, Context vertxContext) {
+    return super.loadData(attributes, tenantId, headers, vertxContext)
+        .compose(superRecordsLoaded -> {
+          log.info("Initializing of tenant's data");
+          TenantLoading tl = new TenantLoading();
+          tl.withKey(REFERENCE_KEY).withLead(REFERENCE_LEAD)
+              .withIdContent()
+              .add("loan-policy-storage/loan-policies")
+              .add("request-policy-storage/request-policies")
+              .add("patron-notice-policy-storage/patron-notice-policies")
+              .add("staff-slips-storage/staff-slips")
+              .withIdRaw()
+              .add("circulation-rules-storage")
+              .withIdContent()
+              .add("cancellation-reason-storage/cancellation-reasons")
+              .withKey(SAMPLE_KEY).withLead(SAMPLE_LEAD)
+              .add("loans", "loan-storage/loans")
+              .add("requests", "request-storage/requests");
+
+          return tl.perform(attributes, headers, vertxContext, superRecordsLoaded);
+        });
+  }
+
+/*  @Validate
   @Override
   public void postTenant(TenantAttributes ta, Map<String, String> headers,
     Handler<AsyncResult<Response>> hndlr, Context cntxt) {
@@ -52,7 +86,7 @@ public class TenantRefAPI extends TenantAPI {
               .respond201WithApplicationJson(null, PostTenantResponse.headersFor201()))));
         });
     }, cntxt);
-  }
+  }*/
 
 /*
   @Override
