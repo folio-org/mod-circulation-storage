9e5b54e60c99c461c79055bcf44d7c3589afce5c
Zak Burke <zburke@cornell.edu>
2021-04-21 10:26:52 -0400

    CIRCSTORE-273 include migration for missing staff slips
    * CIRCSTORE-273 include migration for missing staff slips
    
    The reference data includes four slips (hold, pick-slip,
    request-delivery, and transit) but only two (pick-slip,
    request-delivery) were included in migrations. This meant new tenants
    would have all four slips available but tenants migrated from previous
    releases would only have two.
    
    Refs CIRCSTORE-273
    
    * clean up unnecessary comments
    
    * super helpful if you name your class correctly
    
    * CIRCSTORE-273 Change fromModuleVersion, refactor
    
    Co-authored-by: Alexander Kurash <Oleksandr_Kurash@epam.com>
    (cherry picked from commit 667443265f2a5d427bc8a8bc14dbd2b2f920aeaa)

---
+++ src/test/java/org/folio/rest/api/migration/StaffSlipsHoldTransitMigrationScriptTest.java
@@ -0,0 +1,49 @@
+package org.folio.rest.api.migration;
+
+import static org.junit.Assert.assertThat;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.folio.rest.api.StorageTestSuite;
+import org.folio.rest.support.JsonResponse;
+import org.folio.rest.support.ResponseHandler;
+import org.hamcrest.core.Is;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+public class StaffSlipsHoldTransitMigrationScriptTest extends StaffSlipsMigrationTestBase {
+  public static final String HOLD_ID = "6a6e72f0-69da-4b4c-8254-7154679e9d88";
+  public static final String TRANSIT_ID = "f838cdaf-555a-473f-abf1-f35ef6ab8ae1";
+  private static final String MIGRATION_SCRIPT = loadScript("add_staff_slips_hold_transit.sql");
+
+  @Before
+  public void beforeEach() throws MalformedURLException {
+    StorageTestSuite.deleteAll(staffSlipsStorageUrl(""));
+  }
+
+  @Test
+  public void canMigrateStaffSlips() throws Exception {
+
+    executeMultipleSqlStatements(MIGRATION_SCRIPT);
+
+    CompletableFuture<JsonResponse> getCompleted = new CompletableFuture<>();
+    client.get(staffSlipsStorageUrl(""), StorageTestSuite.TENANT_ID, ResponseHandler.json(getCompleted));
+    JsonResponse getResponse = getCompleted.get(5, TimeUnit.SECONDS);
+
+    assertThat(getResponse.getStatusCode(), Is.is(HttpURLConnection.HTTP_OK));
+
+    JsonArray slipsJsonArray = getResponse.getJson().getJsonArray("staffSlips");
+
+    JsonObject hold = getRecordById(slipsJsonArray, HOLD_ID);
+    JsonObject transit = getRecordById(slipsJsonArray, TRANSIT_ID);
+
+    assertStaffSlip(hold, HOLD_ID, "Hold");
+    assertStaffSlip(transit, TRANSIT_ID, "Transit");
+  }
+}
