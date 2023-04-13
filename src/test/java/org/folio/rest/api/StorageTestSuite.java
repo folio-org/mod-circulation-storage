2dd579a706cf4863312b1b2a8a4326d1a5888ce0
Oleksandr Vidinieiev <Oleksandr_Vidinieiev@epam.com>
2023-04-13 13:50:45 +0300

    CIRCSTORE-407 Do not use constant verticle port

--- src/test/java/org/folio/rest/api/StorageTestSuite.java
+++ src/test/java/org/folio/rest/api/StorageTestSuite.java
@@ -90,7 +90,6 @@ public class StorageTestSuite {
   public static final String TENANT_ID = "test_tenant";
 
   private static Vertx vertx;
-  public static final int VERTICLE_PORT = NetworkUtils.nextFreePort();
   public static final int PROXY_PORT = NetworkUtils.nextFreePort();
   public static final int OKAPI_MOCK_PORT = NetworkUtils.nextFreePort();
   private static boolean initialised = false;
@@ -155,8 +154,11 @@ public class StorageTestSuite {
     System.setProperty("kafka-port", port);
     System.setProperty("kafka-host", host);
 
+    int verticlePort = NetworkUtils.nextFreePort();
+
+
     DeploymentOptions options = new DeploymentOptions();
-    options.setConfig(new JsonObject().put("http.port", VERTICLE_PORT));
+    options.setConfig(new JsonObject().put("http.port", verticlePort));
     startVerticle(options);
 
     mockServer = new MockServer(OKAPI_MOCK_PORT, vertx);
@@ -170,7 +172,7 @@ public class StorageTestSuite {
 
     wireMockServer.stubFor(any(anyUrl())
       .atPriority(10)
-      .willReturn(aResponse().proxiedFrom("http://localhost:" + VERTICLE_PORT)));
+      .willReturn(aResponse().proxiedFrom("http://localhost:" + verticlePort)));
 
     prepareTenant(TENANT_ID, true);
 
