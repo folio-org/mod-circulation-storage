79b1de20cf473445304f1055ff66a7e24166514a
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2023-05-22 15:50:23 +0530

    CIRC-1786 Checkout lock post api implemented

--- src/main/java/org/folio/rest/impl/CheckOutLockAPI.java
+++ src/main/java/org/folio/rest/impl/CheckOutLockAPI.java
@@ -12,6 +12,8 @@ import org.apache.logging.log4j.Logger;
 import org.folio.rest.RestVerticle;
 import org.folio.rest.jaxrs.model.CheckoutLock;
 import org.folio.rest.jaxrs.resource.CheckOutLockStorage;
+import org.folio.rest.persist.PostgresClient;
+import org.folio.rest.tools.client.exceptions.ResponseException;
 import org.folio.support.PgClientFutureAdapter;
 
 import javax.ws.rs.core.Response;
@@ -23,6 +25,7 @@ import java.util.stream.Collectors;
 import static io.vertx.core.Future.succeededFuture;
 import static javax.ws.rs.core.HttpHeaders.CONTENT_TYPE;
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+import static org.folio.rest.persist.PgUtil.postgresClient;
 import static org.folio.support.DbUtil.rowSetToStream;
 
 public class CheckOutLockAPI implements CheckOutLockStorage {
@@ -30,6 +33,7 @@ public class CheckOutLockAPI implements CheckOutLockStorage {
   private static final String CHECK_OUT_LOCK_TABLE = "check_out_lock";
 
   private static final Logger log  = LogManager.getLogger();
+
   @Override
   public void postCheckOutLockStorage(CheckoutLock entity, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     log.info("postCheckOutLockStorage:: entity {}", entity);
@@ -43,8 +47,7 @@ public class CheckOutLockAPI implements CheckOutLockStorage {
           return pgClient.execute(insertSql(entity))
             .map(x -> {
               log.info(" x value {} ",x);
-              List<CheckoutLock> checkoutLocks = this.mapToCheckOutLock(x);
-              return checkoutLocks.isEmpty()?new CheckoutLock():checkoutLocks.get(0);
+              return this.mapToCheckOutLock(x);
             })
             .map(PostCheckOutLockStorageResponse::respond201WithApplicationJson)
             .map(Response.class::cast);
@@ -63,6 +66,10 @@ public class CheckOutLockAPI implements CheckOutLockStorage {
       })
       .onComplete(asyncResultHandler);
   }
+
+//  private Future<CheckoutLock> getCheckOutLockByUserId(String userId, PostgresClient postgresClient) {
+//
+//  }
 
   @Override
   public void getCheckOutLockStorage(int offset, int limit, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
@@ -91,16 +98,15 @@ public class CheckOutLockAPI implements CheckOutLockStorage {
     return String.format("Insert into %s (userid) values('%s')", CHECK_OUT_LOCK_TABLE, checkoutLock.getUserId());
   }
 
-  private List<CheckoutLock> mapToCheckOutLock(RowSet<Row> rowSet) {
-    log.info("Inside rowSet {} ",rowSet);
-    List<CheckoutLock>  checkoutLocks = rowSetToStream(rowSet)
-      .map(row -> new CheckoutLock()
+  private CheckoutLock  mapToCheckOutLock(RowSet<Row> rowSet) {
+    log.info("Inside rowSet {} ",rowSet.iterator().next());
+    var row = rowSet.iterator().next();
+    CheckoutLock  checkoutLock = new CheckoutLock()
       .withId(row.getString("id"))
       .withUserId(row.getString("userId"))
-      .withCreationDate(Date.valueOf(row.getLocalDate("creationDate"))))
-      .collect(Collectors.toList());
-    log.info("mapToCheckOutLock:: checkoutLocks {}", checkoutLocks);
-    return checkoutLocks;
+      .withCreationDate(Date.valueOf(row.getLocalDate("creationDate")));
+    log.info("mapToCheckOutLock:: checkoutLocks {}", checkoutLock);
+    return checkoutLock;
   }
 
   private Response mapExceptionToResponse(Throwable t) {
