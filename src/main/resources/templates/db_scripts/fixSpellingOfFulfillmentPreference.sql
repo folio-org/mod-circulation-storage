784d2958cd31cca643e2b4b82d5936d370996a8c
Roman_Barannyk <roman_barannyk@epam.com>
2023-10-24 13:29:59 +0300

    CIRCSTORE-460 add fulfillmentPreference spelling change script

---
+++ src/main/resources/templates/db_scripts/fixSpellingOfFulfillmentPreference.sql
@@ -0,0 +1,3 @@
+UPDATE ${myuniversity}_${mymodule}.request
+SET jsonb = jsonb - 'fulfilmentPreference' || jsonb_build_object('fulfillmentPreference', jsonb->'fulfilmentPreference')
+WHERE jsonb->'fulfilmentPreference' IS NOT NULL;
