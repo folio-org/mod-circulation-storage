6936d354538996b1ca68d7f413293837cf228a84
Roman_Barannyk <roman_barannyk@epam.com>
2023-11-17 12:41:38 +0200

    CIRCSTORE-465 add test

--- src/main/resources/templates/db_scripts/add_search_slips.sql
+++ src/main/resources/templates/db_scripts/add_search_slips.sql
@@ -1,4 +1,4 @@
-INSERT INTO staff_slips(id, jsonb)
+INSERT INTO ${myuniversity}_${mymodule}.staff_slips(id, jsonb)
 VALUES ('e6e29ec1-1a76-4913-bbd3-65f4ffd94e03',
         jsonb_build_object(
           'id', 'e6e29ec1-1a76-4913-bbd3-65f4ffd94e03',
