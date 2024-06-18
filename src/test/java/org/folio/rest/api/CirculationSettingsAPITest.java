6f4e8bf4c110e3445fd63551feaed3628ee1880c
alexanderkurash <alexander.kurash@gmail.com>
2024-06-18 16:53:52 +0300

    CIRCSTORE-515 Remove context config

--- src/test/java/org/folio/rest/api/CirculationSettingsAPITest.java
+++ src/test/java/org/folio/rest/api/CirculationSettingsAPITest.java
@@ -11,16 +11,13 @@ import java.util.concurrent.TimeoutException;
 import org.folio.rest.support.ApiTests;
 import org.folio.rest.support.http.AssertingRecordClient;
 import org.folio.rest.support.http.InterfaceUrls;
-import org.folio.rest.support.spring.TestContextConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
 
 import io.vertx.core.json.JsonObject;
 import junitparams.JUnitParamsRunner;
 
 @RunWith(JUnitParamsRunner.class)
-@ContextConfiguration(classes = TestContextConfiguration.class)
 public class CirculationSettingsAPITest extends ApiTests {
 
   private final AssertingRecordClient circulationSettingsClient =
