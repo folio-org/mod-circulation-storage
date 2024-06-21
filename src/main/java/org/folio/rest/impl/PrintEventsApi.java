f717c53e6435bddedc78d5e19eca81c31f851c69
SreejaMangarapu <sreeja_mangarapu@epam.com>
2024-06-21 21:51:57 +0530

    CIRCSTORE-513 files modified

--- src/main/java/org/folio/rest/impl/PrintEventsApi.java
+++ src/main/java/org/folio/rest/impl/PrintEventsApi.java
@@ -3,7 +3,7 @@ package org.folio.rest.impl;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Handler;
-import org.folio.rest.jaxrs.model.PrintEvent;
+import org.folio.rest.jaxrs.model.PrintEvents;
 import org.folio.rest.jaxrs.resource.PrintEventsStorage;
 import org.folio.service.BatchPrintEventService;
 import org.slf4j.Logger;
@@ -12,21 +12,20 @@ import org.slf4j.LoggerFactory;
 import javax.ws.rs.core.Response;
 import java.util.Map;
 
+import static io.vertx.core.Future.succeededFuture;
+
 public class PrintEventsApi implements PrintEventsStorage {
   private static final Logger LOG = LoggerFactory.getLogger(PrintEventsApi.class);
   @Override
-  public void postPrintEventsStoragePrintEvents(String lang, PrintEvent printEvent, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-   LOG.info("inside post api ");
+  public void postPrintEventsStoragePrintEvents(PrintEvents printEvents, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    LOG.info("inside post api {}", printEvents);
     new BatchPrintEventService(vertxContext, okapiHeaders)
-      .create(printEvent)
-      .onComplete(asyncResultHandler);
+      .create(printEvents.getPrintEvents())
+      .onSuccess(response -> asyncResultHandler.handle(succeededFuture(response)))
+      .onFailure(throwable -> asyncResultHandler.handle(succeededFuture(PostPrintEventsStoragePrintEventsResponse.respond500WithTextPlain(throwable.getMessage()))));
+  }
   }
 
-  @Override
-  public void getPrintEventsStoragePrintEvents(String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
-
-  }
-}
 
 
 
