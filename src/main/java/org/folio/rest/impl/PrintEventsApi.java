81146d8ca7a90522d12374a2e49e422f2ca2c5af
SreejaMangarapu <sreeja_mangarapu@epam.com>
2024-06-24 14:36:11 +0530

    CIRCSTORE-513 files modified

--- src/main/java/org/folio/rest/impl/PrintEventsApi.java
+++ src/main/java/org/folio/rest/impl/PrintEventsApi.java
@@ -4,6 +4,7 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Handler;
 import org.folio.rest.jaxrs.model.PrintEvents;
+import org.folio.rest.jaxrs.model.PrintEventsRequest;
 import org.folio.rest.jaxrs.resource.PrintEventsStorage;
 import org.folio.service.BatchPrintEventService;
 import org.slf4j.Logger;
@@ -17,10 +18,10 @@ import static io.vertx.core.Future.succeededFuture;
 public class PrintEventsApi implements PrintEventsStorage {
   private static final Logger LOG = LoggerFactory.getLogger(PrintEventsApi.class);
   @Override
-  public void postPrintEventsStoragePrintEvents(PrintEvents printEvents, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+  public void postPrintEventsStoragePrintEvents(PrintEventsRequest printEvents, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     LOG.info("inside post api {}", printEvents);
     new BatchPrintEventService(vertxContext, okapiHeaders)
-      .create(printEvents.getPrintEvents())
+      .create(printEvents)
       .onSuccess(response -> asyncResultHandler.handle(succeededFuture(response)))
       .onFailure(throwable -> asyncResultHandler.handle(succeededFuture(PostPrintEventsStoragePrintEventsResponse.respond500WithTextPlain(throwable.getMessage()))));
   }
