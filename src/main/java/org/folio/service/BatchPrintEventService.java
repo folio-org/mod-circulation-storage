c5c07b0a69cc755675aff9e4a65c489c31173310
SreejaMangarapu <sreeja_mangarapu@epam.com>
2024-06-24 13:01:56 +0530

    CIRCSTORE-513 files modified

--- src/main/java/org/folio/service/BatchPrintEventService.java
+++ src/main/java/org/folio/service/BatchPrintEventService.java
@@ -9,21 +9,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.Response;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
-
 
 import static org.folio.support.ModuleConstants.PRINT_EVENTS_TABLE;
 
 public class BatchPrintEventService {
 
   private static final Logger LOG = LoggerFactory.getLogger(BatchPrintEventService.class);
-  private static final int BATCH_SIZE = 100; // Maximum batch size
-  private static final long BATCH_INTERVAL = 1500; // Batch interval in milliseconds
-
   private final Context vertxContext;
   private final Map<String, String> okapiHeaders;
 
@@ -33,40 +26,9 @@ public class BatchPrintEventService {
     this.okapiHeaders = okapiHeaders;
   }
 
-  public Future<Response> create(List<PrintEvent> printEvent) {
-    LOG.info("inside create batch");
-//    List<PrintEvent> batch = new ArrayList<>();
-//    for(int i=0;i<100;i++) {
-//      PrintEvent pEvent = new PrintEvent();
-//      pEvent.setRequesterId(UUID.randomUUID().toString());
-//      pEvent.setRequestId(UUID.randomUUID().toString());
-//      pEvent.setPrintEventDate(new Date());
-//      batch.add(pEvent);
-//    }
-    return saveBatch(printEvent);
-  }
-
-  private void processBatch(Long timerId) {
-//    List<PrintEvent> batch = new ArrayList<>();
-//
-//    while (!eventQueue.isEmpty() && batch.size() < BATCH_SIZE) {
-//      batch.add(eventQueue.poll());
-//    }
-//
-//    if (!batch.isEmpty()) {
-//      saveBatch(batch).onComplete(ar -> {
-//        if (ar.failed()) {
-//          System.err.println("Batch processing failed: " + ar.cause().getMessage());
-//        }
-//      });
-//    }
-    LOG.info("process batch execution ");
-  }
-
-  private Future<Response> saveBatch(List<PrintEvent> batch) {
-    LOG.info("inside save batch");
-    return PgUtil.postSync(PRINT_EVENTS_TABLE,batch,100,true,okapiHeaders, vertxContext,
+  public Future<Response> create(List<PrintEvent> printEvents) {
+    LOG.info("create:: save print events {}", printEvents);
+    return PgUtil.postSync(PRINT_EVENTS_TABLE,printEvents,10000,true,okapiHeaders, vertxContext,
       PrintEventsStorage.PostPrintEventsStoragePrintEventsResponse.class);
   }
-
 }
