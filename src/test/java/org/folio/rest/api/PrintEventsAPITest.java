a8beb311f19b4af8522997fb9e5632de764ed7b2
SreejaMangarapu <sreeja_mangarapu@epam.com>
2024-06-26 19:20:50 +0530

    CIRCSTORE-513 added PrintEventsAPITest.java

--- src/test/java/org/folio/rest/api/PrintEventsAPITest.java
+++ src/test/java/org/folio/rest/api/PrintEventsAPITest.java
@@ -1,37 +1,71 @@
 package org.folio.rest.api;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.junit.MatcherAssert.assertThat;
 
+import io.vertx.core.json.JsonObject;
+import org.folio.rest.support.ApiTests;
+import org.folio.rest.support.JsonResponse;
+import org.folio.rest.support.http.AssertingRecordClient;
+import org.folio.rest.support.http.InterfaceUrls;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
-import org.folio.rest.support.ApiTests;
-import org.folio.rest.support.http.AssertingRecordClient;
-import org.folio.rest.support.http.InterfaceUrls;
-import org.junit.Test;
+import static org.folio.rest.support.matchers.HttpResponseStatusCodeMatchers.isUnprocessableEntity;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
 
-import io.vertx.core.json.JsonObject;
-public class PrintEventsAPITest extends ApiTests{
+public class PrintEventsAPITest extends ApiTests {
   private final AssertingRecordClient printEventsClient =
     new AssertingRecordClient(
       client, StorageTestSuite.TENANT_ID, InterfaceUrls::printEventsUrl,
-      "circulation-settings");
+      " ");
 
   @Test
   public void canCreatePrintEventLog() throws MalformedURLException, ExecutionException, InterruptedException, TimeoutException {
     String id = UUID.randomUUID().toString();
     JsonObject printEventsJson = getPrintEvent(id);
-    JsonObject printEventsResponse = printEventsClient.create(printEventsJson).getJson();
-    assertThat(printEventsResponse.getString("id"), is(id));
-    assertThat(printEventsResponse.getString("requestIds"),is(printEventsJson.getString("requestIds")));
-    assertThat(printEventsResponse.getString("requesterId"),is(printEventsJson.getString("requesterId")));
-    assertThat(printEventsResponse.getString("requesterName"),is(printEventsJson.getString("requesterName")));
+    JsonResponse response = printEventsClient.attemptCreate(printEventsJson);
+    assertThat(response.getStatusCode(), Is.is(HttpURLConnection.HTTP_CREATED));
   }
+
+  @Test
+  public void createPrintEventLogWithMissingFields() throws MalformedURLException, ExecutionException, InterruptedException, TimeoutException {
+    String id = UUID.randomUUID().toString();
+    List<String> requestIds = List.of("request1", "request2");
+    JsonObject printEventsJson = new JsonObject()
+      .put("id", id)
+      .put("requestIds", requestIds)
+      .put("requesterName", "Sample Requester")
+      .put("printEventDate", "2024-06-25T14:30:00Z");
+    JsonResponse response = printEventsClient.attemptCreate(printEventsJson);
+    assertThat(response, isUnprocessableEntity());
+  }
+
+  @Test
+  public void createPrintEventLogWithBlankFields() throws MalformedURLException, ExecutionException, InterruptedException, TimeoutException {
+    String id = UUID.randomUUID().toString();
+    JsonObject printEventsJson = getPrintEvent(id);
+    printEventsJson.put("requesterId", " ");
+    JsonResponse response = printEventsClient.attemptCreate(printEventsJson);
+    assertThat(response, isUnprocessableEntity());
+  }
+
+  @Test
+  public void createPrintEventLogWhenRequestListIsEmpty() throws MalformedURLException, ExecutionException, InterruptedException, TimeoutException {
+    String id = UUID.randomUUID().toString();
+    List<String> requestIds = List.of();
+    JsonObject printEventsJson = getPrintEvent(id);
+    printEventsJson.put("requestIds", requestIds);
+    JsonResponse response = printEventsClient.attemptCreate(printEventsJson);
+    assertThat(response, isUnprocessableEntity());
+  }
+
   private JsonObject getPrintEvent(String id) {
-    List<String> requestIds = List.of("request1","request2");
+    List<String> requestIds = List.of("request1", "request2");
     return new JsonObject()
       .put("id", id)
       .put("requestIds", requestIds)
