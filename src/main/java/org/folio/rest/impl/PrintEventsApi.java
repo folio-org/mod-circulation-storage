445b474919b22f1fec7200b2de8023c00622d374
SreejaMangarapu <sreeja_mangarapu@epam.com>
2024-07-07 14:10:38 +0530

    CIRCSTORE-513 print-events-storage.raml modified

--- src/main/java/org/folio/rest/impl/PrintEventsApi.java
+++ src/main/java/org/folio/rest/impl/PrintEventsApi.java
@@ -4,7 +4,7 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Context;
 import io.vertx.core.Handler;
 import org.folio.rest.jaxrs.model.PrintEventsRequest;
-import org.folio.rest.jaxrs.resource.PrintEventsStorage;
+import org.folio.rest.jaxrs.resource.PrintEventsStoragePrintEvents;
 import org.folio.service.PrintEventsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,15 +14,16 @@ import java.util.Map;
 
 import static io.vertx.core.Future.succeededFuture;
 
-public class PrintEventsApi implements PrintEventsStorage {
+public class PrintEventsApi implements PrintEventsStoragePrintEvents {
   private static final Logger LOG = LoggerFactory.getLogger(PrintEventsApi.class);
 
   @Override
-  public void postPrintEventsStoragePrintEvents(PrintEventsRequest printEventsRequest, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+  public void postPrintEventsStoragePrintEventsCreateBatch(PrintEventsRequest printEventsRequest, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
     LOG.info("postPrintEventsStoragePrintEvents:: save print events {}", printEventsRequest);
     new PrintEventsService(vertxContext, okapiHeaders)
       .create(printEventsRequest)
       .onSuccess(response -> asyncResultHandler.handle(succeededFuture(response)))
-      .onFailure(throwable -> asyncResultHandler.handle(succeededFuture(PostPrintEventsStoragePrintEventsResponse.respond500WithTextPlain(throwable))));
+      .onFailure(throwable -> asyncResultHandler.handle(succeededFuture(PostPrintEventsStoragePrintEventsCreateBatchResponse.respond500WithTextPlain(throwable.getMessage()))));
+
   }
 }
