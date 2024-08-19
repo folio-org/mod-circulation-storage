46fe4c6943c5f093a0a1851df80e3b8a5844210e
Antony_Hruschev <anton.gitgit@gmail.com>
2024-08-19 21:06:56 +0400

    CIRCSTORE-519 fixed unit test

--- src/test/java/org/folio/rest/api/CirculationSettingsAPITest.java
+++ src/test/java/org/folio/rest/api/CirculationSettingsAPITest.java
@@ -59,8 +59,8 @@ public class CirculationSettingsAPITest extends ApiTests {
     JsonObject circulationSettingsJson = getCirculationSetting(id);
     JsonObject circulationSettingsResponse =
       circulationSettingsClient.create(circulationSettingsJson).getJson();
-    JsonObject circulationSettingsJsonUpdated = getUpdatedSettingsJson();
-    circulationSettingsClient.create(circulationSettingsJsonUpdated);
+//    JsonObject circulationSettingsJsonUpdated = getUpdatedSettingsJson();
+//    circulationSettingsClient.create(circulationSettingsJsonUpdated);
     JsonObject circulationSettingsById = circulationSettingsClient.getById(id).getJson();
 
     assertThat(circulationSettingsResponse.getString(ID_KEY), is(id));
