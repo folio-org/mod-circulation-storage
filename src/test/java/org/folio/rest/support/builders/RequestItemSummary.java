9a80571bd56b1a2d0f308c1bfb41dffdaf162896
Kapil Verma <kapil_verma1@epam.com>
2024-12-05 15:58:50 +0530

    CIRCSTORE-539: fixed PR comments

--- src/test/java/org/folio/rest/support/builders/RequestItemSummary.java
+++ src/test/java/org/folio/rest/support/builders/RequestItemSummary.java
@@ -12,15 +12,28 @@ public class RequestItemSummary {
   final String title;
   final String barcode;
   final List<Pair<UUID, String>> identifiers;
+  final String itemEffectiveLocationId;
+  final String itemEffectiveLocationName;
+  final String retrievalServicePointId;
+  final String retrievalServicePointName;
+
 
   public RequestItemSummary(String title, String barcode) {
     this(title, barcode, Collections.emptyList());
   }
 
   private RequestItemSummary(String title, String barcode, List<Pair<UUID, String>> identifiers) {
+    this(title, barcode, Collections.emptyList(), null, null, null, null);
+  }
+
+  public RequestItemSummary(String title, String barcode, List<Pair<UUID, String>> identifiers, String itemEffectiveLocationId, String itemEffectiveLocationName, String retrievalServicePointId, String retrievalServicePointName) {
     this.title = title;
     this.barcode = barcode;
     this.identifiers = new ArrayList<>(identifiers);
+    this.itemEffectiveLocationId = itemEffectiveLocationId;
+    this.itemEffectiveLocationName = itemEffectiveLocationName;
+    this.retrievalServicePointId = retrievalServicePointId;
+    this.retrievalServicePointName = retrievalServicePointName;
   }
 
   public RequestItemSummary addIdentifier(UUID identifierId, String value) {
@@ -30,6 +43,10 @@ public class RequestItemSummary {
     return new RequestItemSummary(
       this.title,
       this.barcode,
-      copiedIdentifiers);
+      copiedIdentifiers,
+      this.itemEffectiveLocationId,
+      this.itemEffectiveLocationName,
+      this.retrievalServicePointId,
+      this.retrievalServicePointName);
   }
 }
