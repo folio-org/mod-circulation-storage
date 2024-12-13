78f28d05d335debc41db06a1909a1c4e7137e610
Kapil Verma <kapil_verma1@epam.com>
2024-12-13 16:50:03 +0530

    CIRCSTORE-540: added log for debugging

--- src/main/java/org/folio/service/event/handler/ItemUpdateEventHandler.java
+++ src/main/java/org/folio/service/event/handler/ItemUpdateEventHandler.java
@@ -1,11 +1,14 @@
 package org.folio.service.event.handler;
 
+import static org.apache.logging.log4j.LogManager.getLogger;
 import static org.folio.kafka.KafkaHeaderUtils.kafkaHeadersToMap;
 
 import org.apache.commons.collections4.map.CaseInsensitiveMap;
+import org.apache.logging.log4j.Logger;
 import org.folio.kafka.AsyncRecordHandler;
 import org.folio.persist.RequestRepository;
 import org.folio.rest.client.InventoryStorageClient;
+import org.folio.service.event.EntityChangedEventPublisher;
 import org.folio.service.event.handler.processor.ItemUpdateProcessorForRequest;
 
 import io.vertx.core.Context;
@@ -15,7 +18,7 @@ import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
 
 public class ItemUpdateEventHandler implements AsyncRecordHandler<String, String> {
   private final Context context;
-
+  private static final Logger log = getLogger(ItemUpdateEventHandler.class);
   public ItemUpdateEventHandler(Context context) {
     this.context = context;
   }
@@ -25,7 +28,7 @@ public class ItemUpdateEventHandler implements AsyncRecordHandler<String, String
     JsonObject payload = new JsonObject(kafkaConsumerRecord.value());
     CaseInsensitiveMap<String, String> headers =
       new CaseInsensitiveMap<>(kafkaHeadersToMap(kafkaConsumerRecord.headers()));
-
+    log.info("ItemUpdateEventHandler :: handle() :: kafkaHeaders: {}", headers);
     ItemUpdateProcessorForRequest itemUpdateProcessorForRequest =
       new ItemUpdateProcessorForRequest(new RequestRepository(context, headers), new InventoryStorageClient(context.owner(), headers));
 
