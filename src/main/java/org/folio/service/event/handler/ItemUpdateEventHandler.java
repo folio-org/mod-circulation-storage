6d3700b24408fcac545e9b71d758833866017fc5
Kapil Verma <85541312+kapil-epam@users.noreply.github.com>
2024-12-23 14:05:41 +0530

    CIRCSTORE-540: Extended ItemUpdateProcessorForRequest to sync item location and SP updates in Request records (#503)
    * CIRCSTORE-540: ItemUpdateProcessorForRequest update item's location and SP
    
    * CIRCSTORE-540: added log for debugging
    
    * CIRCSTORE-540: added log for debugging
    
    * CIRCSTORE-540: set additionalProperties true inside location.json
    
    * CIRCSTORE-540: fix exception issue
    
    * CIRCSTORE-540: fix exception issue
    
    * CIRCSTORE-540: 1) added async mechanism in EventProcessor class to execute and support non-blocking api calls. 2) Added test case.
    
    * CIRCSTORE-540: removed unnecessary log and code
    
    * CIRCSTORE-540: using location and SP constants
    
    * CIRCSTORE-540: fix NLP due to logging of null object
    
    * CIRCSTORE-540: fixed sonar issues
    
    * CIRCSTORE-540: added interface dependency for location and service-point api

--- src/main/java/org/folio/service/event/handler/ItemUpdateEventHandler.java
+++ src/main/java/org/folio/service/event/handler/ItemUpdateEventHandler.java
@@ -5,6 +5,7 @@ import static org.folio.kafka.KafkaHeaderUtils.kafkaHeadersToMap;
 import org.apache.commons.collections4.map.CaseInsensitiveMap;
 import org.folio.kafka.AsyncRecordHandler;
 import org.folio.persist.RequestRepository;
+import org.folio.rest.client.InventoryStorageClient;
 import org.folio.service.event.handler.processor.ItemUpdateProcessorForRequest;
 
 import io.vertx.core.Context;
@@ -14,7 +15,6 @@ import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
 
 public class ItemUpdateEventHandler implements AsyncRecordHandler<String, String> {
   private final Context context;
-
   public ItemUpdateEventHandler(Context context) {
     this.context = context;
   }
@@ -24,9 +24,8 @@ public class ItemUpdateEventHandler implements AsyncRecordHandler<String, String
     JsonObject payload = new JsonObject(kafkaConsumerRecord.value());
     CaseInsensitiveMap<String, String> headers =
       new CaseInsensitiveMap<>(kafkaHeadersToMap(kafkaConsumerRecord.headers()));
-
     ItemUpdateProcessorForRequest itemUpdateProcessorForRequest =
-      new ItemUpdateProcessorForRequest(new RequestRepository(context, headers));
+      new ItemUpdateProcessorForRequest(new RequestRepository(context, headers), new InventoryStorageClient(context.owner(), headers));
 
     return itemUpdateProcessorForRequest.run(kafkaConsumerRecord.key(), payload);
   }
