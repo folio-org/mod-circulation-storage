6818a82cde36b86128fa2da987875478f6d49f0d
Kapil Verma <85541312+kapil-epam@users.noreply.github.com>
2024-12-23 14:05:41 +0530

    CIRCSTORE-540: Extended ItemUpdateProcessorForRequest to sync item location and SP updates in Request records (#503)
    * CIRCSTORE-540: ItemUpdateProcessorForRequest update item's location and SP
    
    * CIRCSTORE-540: added log for debugging
    
    * CIRCSTORE-540: added log for debugging
    
    * CIRCSTORE-540: set additionalProperties true inside location.json
    
    * CIRCSTORE-540: fix exception issue
    
    * CIRCSTORE-540: fix exception issue
    
    * CIRCSTORE-540: 1) added async mechanism in EventProcessor class to execute and support non-blocking api calls. 2) Added test case.
    
    * CIRCSTORE-540: removed unnecessary log and code
    
    * CIRCSTORE-540: using location and SP constants
    
    * CIRCSTORE-540: fix NLP due to logging of null object
    
    * CIRCSTORE-540: fixed sonar issues
    
    * CIRCSTORE-540: added interface dependency for location and service-point api

--- src/main/java/org/folio/rest/client/InventoryStorageClient.java
+++ src/main/java/org/folio/rest/client/InventoryStorageClient.java
@@ -3,6 +3,7 @@ package org.folio.rest.client;
 import java.util.Collection;
 import java.util.Map;
 
+import org.folio.rest.jaxrs.model.Location;
 import org.folio.rest.jaxrs.model.Servicepoint;
 
 import io.vertx.core.Future;
@@ -12,12 +13,19 @@ public class InventoryStorageClient extends OkapiClient {
   private static final String SERVICE_POINTS_URL = "/service-points";
   private static final String SERVICE_POINTS_COLLECTION_NAME = "servicepoints";
 
+  private static final String LOCATION_URL = "/locations";
+  private static final String LOCATION_COLLECTION_NAME = "locations";
+
   public InventoryStorageClient(Vertx vertx, Map<String, String> okapiHeaders) {
     super(vertx, okapiHeaders);
   }
 
   public Future<Collection<Servicepoint>> getServicePoints(Collection<String> ids) {
     return get(SERVICE_POINTS_URL, ids, SERVICE_POINTS_COLLECTION_NAME, Servicepoint.class);
+  }
+
+  public Future<Collection<Location>> getLocations(Collection<String> ids) {
+    return get(LOCATION_URL, ids, LOCATION_COLLECTION_NAME, Location.class);
   }
 
 }
