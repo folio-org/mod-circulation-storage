69fdd70404d6e045dae1aa2ad9e525db80c7c96b
alexanderkurash <alexander.kurash@gmail.com>
2026-03-26 19:47:33 +0200

    CIRCSTORE-642 Move cache update

--- src/main/java/org/folio/service/event/handler/LocationUpdateEventHandler.java
+++ src/main/java/org/folio/service/event/handler/LocationUpdateEventHandler.java
@@ -4,6 +4,8 @@ import static org.folio.kafka.KafkaHeaderUtils.kafkaHeadersToMap;
 import static org.folio.rest.RestVerticle.OKAPI_HEADER_TENANT;
 
 import org.apache.commons.collections4.map.CaseInsensitiveMap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.kafka.AsyncRecordHandler;
 import org.folio.persist.RequestRepository;
 import org.folio.rest.client.InventoryStorageClient;
@@ -16,6 +18,7 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
 
 public class LocationUpdateEventHandler implements AsyncRecordHandler<String, String> {
+  private static final Logger log = LogManager.getLogger();
 
   private final Context context;
 
@@ -30,16 +33,19 @@ public class LocationUpdateEventHandler implements AsyncRecordHandler<String, St
       new CaseInsensitiveMap<>(kafkaHeadersToMap(kafkaConsumerRecord.headers()));
     String tenantId = headers.get(OKAPI_HEADER_TENANT);
 
-    // Invalidate cache for updated location
-    JsonObject newObject = payload.getJsonObject("new");
-    if (newObject != null && newObject.containsKey("id")) {
-      InventoryStorageClient.updateLocationCache(tenantId, newObject.getString("id"),
-        newObject.mapTo(Location.class));
-    }
-
     ItemLocationUpdateProcessorForRequest itemLocationUpdateProcessorForRequest =
       new ItemLocationUpdateProcessorForRequest(new RequestRepository(context, headers));
 
-    return itemLocationUpdateProcessorForRequest.run(kafkaConsumerRecord.key(), payload);
+    return itemLocationUpdateProcessorForRequest.run(kafkaConsumerRecord.key(), payload)
+      .onComplete(notUsed -> {
+        // Update location cache
+        JsonObject newObject = payload.getJsonObject("new");
+        if (newObject != null && newObject.containsKey("id")) {
+          log.info("handle:: updating location cache for tenantId: {}, locationId: {}",
+            tenantId, newObject.getString("id"));
+          InventoryStorageClient.updateLocationCache(tenantId, newObject.getString("id"),
+            newObject.mapTo(Location.class));
+        }
+      });
   }
 }
--- src/main/java/org/folio/service/event/handler/ServicePointUpdateEventHandler.java
+++ src/main/java/org/folio/service/event/handler/ServicePointUpdateEventHandler.java
@@ -4,10 +4,13 @@ import static org.folio.kafka.KafkaHeaderUtils.kafkaHeadersToMap;
 import static org.folio.rest.RestVerticle.OKAPI_HEADER_TENANT;
 
 import org.apache.commons.collections4.map.CaseInsensitiveMap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.kafka.AsyncRecordHandler;
 import org.folio.persist.RequestPolicyRepository;
 import org.folio.persist.RequestRepository;
 import org.folio.rest.client.InventoryStorageClient;
+import org.folio.rest.jaxrs.model.Location;
 import org.folio.rest.jaxrs.model.Servicepoint;
 import org.folio.service.event.handler.processor.ItemRetrievalServicePointUpdateProcessorForRequest;
 import org.folio.service.event.handler.processor.ServicePointUpdateProcessorForRequest;
@@ -19,6 +22,8 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
 
 public class ServicePointUpdateEventHandler implements AsyncRecordHandler<String, String> {
+  private static final Logger log = LogManager.getLogger();
+
   private final Context context;
 
   public ServicePointUpdateEventHandler(Context context) {
@@ -31,12 +36,6 @@ public class ServicePointUpdateEventHandler implements AsyncRecordHandler<String
     var headers = new CaseInsensitiveMap<>(kafkaHeadersToMap(kafkaConsumerRecord.headers()));
     String tenantId = headers.get(OKAPI_HEADER_TENANT);
 
-    JsonObject newObject = payload.getJsonObject("new");
-    if (newObject != null && newObject.containsKey("id")) {
-      InventoryStorageClient.updateServicePointCache(tenantId, newObject.getString("id"),
-        newObject.mapTo(Servicepoint.class));
-    }
-
     var requestRepository = new RequestRepository(context, headers);
     var requestPolicyRepository = new RequestPolicyRepository(context, headers);
 
@@ -45,6 +44,16 @@ public class ServicePointUpdateEventHandler implements AsyncRecordHandler<String
       .compose(notUsed -> new ServicePointUpdateProcessorForRequestPolicy(requestPolicyRepository)
         .run(kafkaConsumerRecord.key(), payload))
       .compose(notUsed -> new ItemRetrievalServicePointUpdateProcessorForRequest(requestRepository)
-        .run(kafkaConsumerRecord.key(), payload));
+        .run(kafkaConsumerRecord.key(), payload))
+      .onComplete(notUsed -> {
+        // Update service point cache
+        JsonObject newObject = payload.getJsonObject("new");
+        if (newObject != null && newObject.containsKey("id")) {
+          log.info("handle:: updating service point cache for tenantId: {}, servicePointId: {}",
+            tenantId, newObject.getString("id"));
+          InventoryStorageClient.updateServicePointCache(tenantId, newObject.getString("id"),
+            newObject.mapTo(Servicepoint.class));
+        }
+      });
   }
 }
