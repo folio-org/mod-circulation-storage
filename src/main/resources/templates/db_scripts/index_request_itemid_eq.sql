90760fc492665b08c5eed9ad8274965d29cd54e9
Roman_Barannyk <roman_barannyk@epam.com>
2026-03-30 13:04:49 +0300

    CIRCSTORE-643 remove redundant script

--- src/main/resources/templates/db_scripts/index_request_itemid_eq.sql
+++
@@ -1,8 +0,0 @@
-    DO $do$
-    BEGIN
-      PERFORM rmb_internal_index(
-      'request',                    'request_itemid_eq_idx', 'ADD',
-      'CREATE INDEX IF NOT EXISTS request_itemid_eq_idx ON ${myuniversity}_${mymodule}.request '
-      || $rmb$((jsonb->>'itemId'))$rmb$);
-    END $do$;
-
