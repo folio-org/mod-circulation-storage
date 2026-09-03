d541ee3b2517a82ac4880cccd9ea73015e441f67
Mukhiddin Yusupov <mukhiddin_yusupov@epam.com>
2026-09-03 17:03:48 +0500

    refactor: DomainEventPublisher extends AbstractEventPublisher

--- src/main/java/org/folio/service/event/DomainEventPublisher.java
+++ src/main/java/org/folio/service/event/DomainEventPublisher.java
@@ -5,88 +5,43 @@ import static org.apache.logging.log4j.LogManager.getLogger;
 import java.util.Map;
 
 import org.apache.logging.log4j.Logger;
-import org.folio.kafka.KafkaConfig;
 import org.folio.kafka.KafkaProducerManager;
-import org.folio.kafka.SimpleKafkaProducerManager;
-import org.folio.kafka.services.KafkaEnvironmentProperties;
 import org.folio.kafka.services.KafkaProducerRecordBuilder;
 import org.folio.rest.tools.utils.TenantTool;
 
 import io.vertx.core.Context;
 import io.vertx.core.Future;
-import io.vertx.kafka.client.producer.KafkaProducer;
 import io.vertx.kafka.client.producer.KafkaProducerRecord;
 
-public class DomainEventPublisher<K, T> {
+public class DomainEventPublisher<K, T> extends AbstractEventPublisher {
 
   private static final Logger log = getLogger(DomainEventPublisher.class);
 
-  private final String kafkaTopic;
-  private final KafkaProducerManager producerManager;
   private final FailureHandler failureHandler;
 
   DomainEventPublisher(String kafkaTopic, KafkaProducerManager producerManager,
       FailureHandler failureHandler) {
-    this.kafkaTopic = kafkaTopic;
-    this.producerManager = producerManager;
+    super(kafkaTopic, producerManager);
     this.failureHandler = failureHandler;
   }
 
-  public DomainEventPublisher(Context vertxContext, String kafkaTopic, FailureHandler failureHandler) {
+  public DomainEventPublisher(Context vertxContext, String kafkaTopic,
+      FailureHandler failureHandler) {
     this(kafkaTopic, createProducerManager(vertxContext), failureHandler);
   }
 
   public Future<Void> publish(K key, DomainEvent<T> event, Map<String, String> okapiHeaders) {
     log.info("publish:: key = {}, eventId = {}, type = {}, topic = {}", key, event.getId(),
-      event.getType(), kafkaTopic);
+        event.getType(), kafkaTopic);
 
     KafkaProducerRecord<K, String> producerRecord =
-      new KafkaProducerRecordBuilder<K, DomainEvent<T>>(TenantTool.tenantId(okapiHeaders))
-        .key(key)
-        .value(event)
-        .topic(kafkaTopic)
-        .propagateOkapiHeaders(okapiHeaders)
-        .build();
-    KafkaProducer<K, String> producer = null;
-    try {
-      producer = getOrCreateProducer();
-      log.info("publish:: Producer created, sending the record...");
+        new KafkaProducerRecordBuilder<K, DomainEvent<T>>(TenantTool.tenantId(okapiHeaders))
+            .key(key)
+            .value(event)
+            .topic(kafkaTopic)
+            .propagateOkapiHeaders(okapiHeaders)
+            .build();
 
-      producer.send(producerRecord)
-        .onSuccess(r -> log.info("publish:: Succeeded sending domain event with key [{}]", key))
-        .onFailure(cause -> {
-          log.error("publish:: Unable to send domain event with key [{}]", key, cause);
-          failureHandler.handle(cause, producerRecord);
-        })
-        .eventually(producer::flush)
-        .eventually(producer::close);
-    } catch (Exception e) {
-      log.error("publish:: Failed to initiate send for domain event with key [{}]", key, e);
-      if (producer != null) {
-        log.info("publish:: Producer is not null, trying to close. Event key: {}.", key);
-        producer.close();
-      }
-      failureHandler.handle(e, producerRecord);
-    }
-
-    return Future.succeededFuture();
+    return sendRecord(producerRecord, key, failureHandler);
   }
-
-  private KafkaProducer<K, String> getOrCreateProducer() {
-    return getOrCreateProducer("");
-  }
-
-  private KafkaProducer<K, String> getOrCreateProducer(String prefix) {
-    return producerManager.createShared(prefix + kafkaTopic);
-  }
-
-  private static KafkaProducerManager createProducerManager(Context vertxContext) {
-    var kafkaConfig = KafkaConfig.builder()
-        .kafkaPort(KafkaEnvironmentProperties.port())
-        .kafkaHost(KafkaEnvironmentProperties.host())
-        .build();
-
-    return new SimpleKafkaProducerManager(vertxContext.owner(), kafkaConfig);
-  }
-
 }
