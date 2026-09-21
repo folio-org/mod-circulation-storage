df13ef2ba77769c3aaeb57b46b4934cc51d8838d
Katherine Bargar <kbargar@fivecolleges.edu>
2026-09-21 14:23:47 +0000

    Validate that requesterid is a uuid and conditionally required

--- src/main/java/org/folio/rest/impl/util/RequestsApiUtil.java
+++ src/main/java/org/folio/rest/impl/util/RequestsApiUtil.java
@@ -7,6 +7,7 @@ import org.folio.rest.jaxrs.model.Error;
 import org.folio.rest.jaxrs.model.Errors;
 import org.folio.rest.jaxrs.model.Request;
 import org.folio.rest.jaxrs.model.Request.RequestLevel;
+import org.folio.support.UUIDValidation;
 
 /**
  * Utility methods for Requests resource.
@@ -56,6 +57,15 @@ public class RequestsApiUtil {
         "Title level request must have both itemId and holdingsRecordId or neither"));
     }
 
+    if (isOpenAndHasNoRequesterId(request)) {
+      errorList.add(createError(
+          "Open request must have a requester ID"));
+    }
+    if (request.getRequesterId() != null &&
+        !UUIDValidation.isValidUUID(request.getRequesterId())) {
+      errorList.add(createError( "Invalid requester ID, should be a UUID"));
+    }
+
     return new Errors().withErrors(errorList);
   }
 
@@ -63,6 +73,11 @@ public class RequestsApiUtil {
     boolean isHoldingsRecordIdAbsent) {
 
     return isItemIdAbsent ^ isHoldingsRecordIdAbsent;
+  }
+
+  private static boolean isOpenAndHasNoRequesterId(Request request) {
+    return request.getStatus().value().startsWith("Open -")
+        && request.getRequesterId() == null;
   }
 
   private static Error createError(String message){
